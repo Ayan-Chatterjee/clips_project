33189816a3531e523c6d4751d33de49c84762610
Ayan <ayanchatterjee0510@gmail.com>
2024-11-10 16:05:32 +0530

    8.001 to 8.008

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  credentials = {
+    email:'',
+    password:''
+  }
 
+  login()
+  {
+    console.log(this.credentials);
+  }
 }
