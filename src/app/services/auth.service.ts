e725b2adc1ce289152bfdf2a53339a574093ec3e
Ayan-Chatterjee <ayanchatterjee0510@gmail.com>
2024-12-04 15:05:55 +0530

    Last changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
-import { Observable,of } from 'rxjs';
-import {delay, filter, map,switchMap} from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { delay, filter, map, switchMap } from 'rxjs/operators';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { json } from 'stream/consumers';
 
@@ -11,76 +14,66 @@ interface RouteData {
   authOnly?: boolean;
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private userCollection: AngularFirestoreCollection<IUser>;
-  public isAuthenticated$ : Observable<boolean>;
-  public isAuthenticatedWithDelay$ : Observable<boolean>;
+  public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
   private redirect = false;
-
-  
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore,
     private router: Router,
     private route: ActivatedRoute
-  ) { 
+  ) {
     this.userCollection = this.db.collection<IUser>('users');
-    this.isAuthenticated$ = auth.user.pipe(
-      map(user => !!user)
-    )
-    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
-      delay(1000)
-    )
-    
+    this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
+
     this.router.events
-  .pipe(
-    filter((e) => e instanceof NavigationEnd),
-    map(() => this.route.firstChild), // Ensure it picks the first child route
-    switchMap((route) => route?.data ?? of<RouteData>({})), // Default to an empty object if no data
-  )
-  .subscribe((data) => {
-    this.redirect = data?.authOnly ?? false; // Use optional chaining for safety
-  });
+      .pipe(
+        filter((e) => e instanceof NavigationEnd),
+        map(() => this.route.firstChild), // Ensure it picks the first child route
+        switchMap((route) => route?.data ?? of<RouteData>({})) // Default to an empty object if no data
+      )
+      .subscribe((data) => {
+        this.redirect = data?.authOnly ?? false; // Use optional chaining for safety
+      });
   }
 
-  public async createsUser(userData:IUser){
-
-    if(!userData.password){
+  public async createsUser(userData: IUser) {
+    if (!userData.password) {
       throw new Error('Password is required');
     }
 
     const userCred = await this.auth.createUserWithEmailAndPassword(
-      userData.email as string, 
+      userData.email as string,
       userData.password as string
-    )
-    if(!userCred.user)
-    {
+    );
+    if (!userCred.user) {
       throw new Error('User can not be found');
     }
     await this.userCollection.doc(userCred.user.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
-      phoneNumber: userData.phoneNumber
-    })
+      phoneNumber: userData.phoneNumber,
+    });
 
     await userCred.user.updateProfile({
-      displayName: userData.name
-    })
+      displayName: userData.name,
+    });
   }
-  async logout($event?: Event){
-    if($event){
-        $event.preventDefault();
+  async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
     }
     await this.auth.signOut();
-    if(this.redirect){
-     await this.router.navigateByUrl('/');
+    if (this.redirect) {
+      await this.router.navigateByUrl('/');
     }
   }
-
 }
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import {v4 as uuid} from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -11,6 +14,16 @@ export class UploadComponent {
   file: File | null = null;
   nextStep = false;
 
+  constructor(private storage: AngularFireStorage) {}
+
+  title = new FormControl('',[
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
   storeFile($event: Event){
     this.isDragOver = false;
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
@@ -19,6 +32,16 @@ export class UploadComponent {
       alert('Please upload a valid MP4 video file');
       return;
     }
+
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/,'')
+    )
     this.nextStep = true;
+  }
+
+  uploadFile(){
+    const clipFileName = uuid()
+    const clipPath = `clips/${this.file?.name}.mp4`;
+    this.storage.upload(clipPath,this.file,);
   }
 }
